5d9147eb2fe363c9569bc8d1c79b1167366741e3
varchodi <rauschenrobert@gmail.com>
2024-06-09 21:16:03 +0300

    Agency: User Invits server actions

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,3 +1,4 @@
+import { getAuthUserDetails, verifyAndAcceptInvitation } from '@/lib/queries';
 import { currentUser } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation';
 import React from 'react'
@@ -5,8 +6,10 @@ import React from 'react'
 const Page = async() => {
   const authAuser = await currentUser();
   if (!authAuser) redirect('/sign-in');
+  const agencyId = await verifyAndAcceptInvitation();
 
-  //get user details 
+  //get user details (with server actions /??)
+  const user = await getAuthUserDetails();
   return (
     <div>
       <h1>Agency DashBoard</h1>
