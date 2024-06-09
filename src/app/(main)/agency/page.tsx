3d27daccc3401e76e0d499281423c0f52db9d899
varchodi <rauschenrobert@gmail.com>
2024-06-09 20:14:15 +0300

    Agency: Redirect user to sign in if not authenticated

--- src/app/(main)/agency/page.tsx
+++ src/app/(main)/agency/page.tsx
@@ -1,6 +1,12 @@
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-const Page = () => {
+const Page = async() => {
+  const authAuser = await currentUser();
+  if (!authAuser) redirect('/sign-in');
+
+  //get user details 
   return (
     <div>
       <h1>Agency DashBoard</h1>
